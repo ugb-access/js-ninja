4246c53f967186ae429f63c2350cfb8de53debc2
Usman Ghani <usmanghanibaig2@gmail.com>
2020-03-13 11:51:47 +0500

    null and undefined data types

---
+++ null&undefined-datatype/index.js
@@ -0,0 +1,7 @@
+let age; // declared but not defined
+
+console.log(age , age + 1, `Age is ${age}`);
+
+let name = null; //  set by intention
+
+console.log(name + 1); // null will be considered as 0, so the answer will be 1;
