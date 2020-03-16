99f7ed27d03f2fcc9920e606fd4d5ff749918d84
Usman Ghani <usmanghanibaig2@gmail.com>
2020-03-16 12:17:13 +0500

    lesson 1 boolean & loose & strict comparison completed

---
+++ boolean-datatype/index.js
@@ -0,0 +1,47 @@
+// boolean true and false
+
+console.log(true, false, 'true', 'false');
+
+// methods can returns true and false
+
+let email = "usmanghanibaig2@gmail.com";
+
+let listBestSingers = ['NSK', 'AMK', 'MH']
+
+// let result = email.includes('usm');
+
+let result = listBestSingers.includes('NSK')
+
+console.log(result);
+
+// comparison operators
+
+console.log( 1 == true ); // 1 is consider as true in numbers and 0 is false
+
+console.log( !1 == false ); 
+
+const age = 25;
+
+if(age) { // if age exist
+    console.log('Its here')
+}
+
+console.log(age >! 25);
+
+console.log(age <= 25);
+
+console.log(age == 25);
+
+console.log(age != 25);
+
+console.log(age >= 25)
+
+console.log(age <= 25);
+
+console.log('ef' > 'abc'); //  true because ef comes after abc so it's greater than a
+
+console.log('abc' < 'ef'); // false because of similar reason
+
+console.log('abc' == 'Abc'); // false because js is case sensitive
+
+
---
+++ loose&strinct-comparison/index.js
@@ -0,0 +1,21 @@
+const age = 30;
+
+
+// loose comparison  (differnt types can be equal)
+console.log(age  == 30);
+
+console.log(age == '30');
+
+console.log(age != 30);
+
+console.log(age != '30');
+
+// strict comaprison (different types cannot be equal)
+
+console.log(age === 30);
+
+console.log(age === '30'); //false beacuse js will not do type conversion in this scenerio
+
+console.log(age !== 30);
+
+console.log(age !== '30');
